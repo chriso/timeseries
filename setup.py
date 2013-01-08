a5769e2ca9a171b67ec15094bf3d886e87b8b9bb
Chris O'Hara <cohara87@gmail.com>
2013-01-08 18:50:51 +1100

    0.3.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "timeseries",
-    version = "0.3.1",
+    version = "0.3.2",
     author = "Chris O'Hara",
     author_email = "cohara87@gmail.com",
     description = "Time series modelling and analysis",
